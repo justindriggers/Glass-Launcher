3b141bc68ff7aa41c3c14b4b4c55a914be1ebca5
Justin Driggers <justin@justindriggers.com>
2013-12-10 19:00:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Glass Launcher
+===========
+
+Glass Launcher is a simple application launcher for Google Glass.
+
+The goal of Glass Launcher is to make Google Glass a more practical device for the average user.
+
+Why should I use this over other application launchers?
+There are currently two other solutions for launching applications. One of them involves installing the AOSP Launcher2 APK, which is designed for typical Android devices, making it difficult to use on Google Glass. The other intercepts an event and starts its own activity in place of the normal Settings activity.
+Recently, Google allowed developers access to the Glass Development Kit (GDK), allowing deeper access into the platform's APIs. Glass Launcher is built with the GDK and gives the user a truly native feel when launching an application.
+
+Use of this source code assumes that you know how to build and install applications to your Google Glass device. To install, simply build and run as you would any other application. After starting the first time, the service will start on each boot, creating a Glass Launcher card to the left of the Time card.
+
+Building this application requires the Android SDK, with both the Android 4.0.3 SDK Platform (API 15) and the Glass Development Kit Sneap Peek installed.
+
+Copyright 2013 Justin Driggers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/com/jtxdriggers/android/glass/glasslauncher/MenuActivity.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2013 Justin Driggers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jtxdriggers.android.glass.glasslauncher;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuActivity extends Activity {
+	
+	public static final String UPDATE_RECEIVER = "com.justindriggers.android.glass.glasslauncher.UPDATE_RECEIVER";
+	
+	private ApplicationsUpdateReceiver mApplicationsReceiver = new ApplicationsUpdateReceiver();
+	private ArrayList<ApplicationInfo> mApplications;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		registerReceiver(mApplicationsReceiver, new IntentFilter(UPDATE_RECEIVER));
+	    
+	    mApplications = GlassLauncherService.getApplications();
+		
+	}
+
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    
+	    openOptionsMenu();
+	}
+	
+	@Override
+	public void onDestroy() {
+		unregisterReceiver(mApplicationsReceiver);
+		
+		super.onDestroy();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (mApplications.isEmpty()) {
+			MenuItem item = menu.add(Menu.NONE, -1, Menu.NONE, "No Apps Found");
+			item.setIcon(R.drawable.ic_warning_50);
+		} else {
+			ApplicationInfo app;
+			MenuItem item;
+			
+			for (int i = 0; i < mApplications.size(); i++) {
+				app = mApplications.get(i);
+				item = menu.add(Menu.NONE, i, i+1, app.title);
+				item.setIcon(app.icon);
+			}
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == -1) {
+			return true;
+		}
+		
+		Intent intent = mApplications.get(item.getItemId()).intent;
+		startActivity(intent);
+		
+		return true;
+	}
+	
+	@Override
+	public void onOptionsMenuClosed(Menu menu) {
+	    finish();
+	}
+	
+	private class ApplicationsUpdateReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent arg1) {
+			startActivity(new Intent(context, MenuActivity.class));
+		}
+	}
+
+}
